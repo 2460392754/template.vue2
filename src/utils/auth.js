67a12f7543e5aef899f8e148bc0e038197b0bdf1
2460392754 <2460392754@qq.com>
2020-02-12 10:37:55 +0800

    update：更新了 auth 的注释

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,17 +1,22 @@
-const TOKEN_KEY = 'token';
+/** token 键名 */
+const TOKEN_KEY = 'TOKEN';
 
+/** localStorage是否有token */
 export const hasToken = function() {
     return getToken() !== 'null';
 };
 
+/** 获取 token */
 export const getToken = function() {
     return localStorage.getItem(TOKEN_KEY);
 };
 
-export const setToken = function(token) {
-    return localStorage.setItem(TOKEN_KEY, token);
+/** 设置 token */
+export const setToken = function(value) {
+    return localStorage.setItem(TOKEN_KEY, value);
 };
 
+/** 删除 token */
 export const removeToken = function() {
     return localStorage.removeItem(TOKEN_KEY);
 };
